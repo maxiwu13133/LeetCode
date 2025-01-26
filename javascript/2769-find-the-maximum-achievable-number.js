de17e4518cd435527c0c4dc854fd0ce6157a615d
max <maximilianwu0828@gmail.com>
2025-01-26 13:18:39 -0800

    2769. Find the Maximum Achievable Number

---
+++ javascript/2769-find-the-maximum-achievable-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+  return num + (t * 2)
+};
+
+console.log(theMaximumAchievableX(4, 1));
+console.log(theMaximumAchievableX(3, 2));
