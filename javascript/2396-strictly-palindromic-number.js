3980986fe85af2b70c6198092f61edd3dacd2676
max <maximilianwu0828@gmail.com>
2025-02-15 20:34:34 -0800

    2396. Strictly Palindromic Number

---
+++ javascript/2396-strictly-palindromic-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isStrictlyPalindromic = function(n) {
+  return false;
+};
+
+console.log(isStrictlyPalindromic(n = 9));  // false
+console.log(isStrictlyPalindromic(n = 4));  // false
