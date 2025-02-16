ece016007699f36ab8e13777f93a459c208b27af
max <maximilianwu0828@gmail.com>
2025-02-15 20:28:19 -0800

    2235. Add Two Integers

---
+++ javascript/2235-add-two-integers.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+  return num1 + num2;
+};
+
+console.log(sum(num1 = 12, num2 = 5));   // 17
+console.log(sum(num1 = -10, num2 = 4));  // -6
