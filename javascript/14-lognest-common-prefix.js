2d6e99a8f2e853111b54a976b090a31ed1618c69
max <maximilianwu0828@gmail.com>
2025-02-20 13:49:18 -0800

    14. Longest Common Prefix

---
+++ javascript/14-lognest-common-prefix.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  const prefix = [...strs[0]];
+
+  for (let i = 1; i < strs.length; i++) {
+    for (let j = 0; j < Math.max(strs[i].length, prefix.length); j++) {
+      if (prefix[j] !== strs[i][j]) {
+        prefix.splice(j)
+      }
+    }
+  }
+
+  return prefix.join("");
+};
+
+// console.log(longestCommonPrefix(strs = ["flower","flow","flight"]));  // fl
+// console.log(longestCommonPrefix(strs = ["dog","racecar","car"]));     // 
+console.log(longestCommonPrefix(strs = ["ab", "a"]));     // 
